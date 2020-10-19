1a3fdd4c8b62312e1a9bbea286aa53c1b3a28e5b
Ziyi Lu <lzy-1006@berkeley.edu>
2020-10-19 16:03:18 -0700

    Exemplary release commit

--- statistics.py
+++ statistics.py
@@ -1,6 +1,3 @@
 # Exemplary statistical code for Astro 120, FW2020
-# I think it would be cleaner to separate functions of different natures
 
-# This is a development branch that is not for students
-# git checkout lab3UrgentNeed
-# 2020-10-19
+# This is the release branch that is for students
