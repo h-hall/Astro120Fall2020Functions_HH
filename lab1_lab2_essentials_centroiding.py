a1361bee311703a4b7045c05905410f144829b71
lzy-1006 <lzy-1006@berkeley.edu>
2020-11-01 18:13:43 -0800

    Centroiding: moved parameters out

--- lab1_lab2_essentials_centroiding.py
+++ lab1_lab2_essentials_centroiding.py
@@ -1,22 +1,22 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # coding: utf-8
 
 # In[1]:
 
-
-#Import all important packages
+# Import all important packages
 import numpy as np
-
 
 # In[2]:
 
+# Define parameters
+CCD_PIXELS = 2048
 
 def centroiding(signal_peaks_index, signal_data): # For this function, the first input is the array of peak indeces that was produced in our peak finding function
-    CCD_PIXELS = 2048
     pixel = np.arange(0, CCD_PIXELS)
     n_peaks = len(signal_peaks_index)
     centroids = np.zeros((n_peaks,)) # Pixel coordinates for all the centroids
     FWHM = np.zeros((n_peaks,)) # Full-width-half-max signal values for each centroid
+
     for i in range(n_peaks): # We are using the pixel indeces from our original peak finding function
         peak = signal_peaks_index[i]
         half_max = signal_data[peak] / 2.
@@ -26,8 +26,9 @@ def centroiding(signal_peaks_index, signal_data): # For this function, the first
         xmax = np.nonzero(signal_data[peak:-1] <= half_max)[0][0]
         x_range = pixel[peak - xmin:peak + xmax].copy()
         I_range = signal_data[peak - xmin:peak + xmax].copy()
-        I_total = np.sum(I_range) # Avoid duplicate calculation
+        I_total = np.sum(I_range)
         x_com = np.sum(x_range * I_range / I_total) # x_com stands for X Center of Mass
         centroids[i] = (x_com)
+
     return centroids, FWHM
 
