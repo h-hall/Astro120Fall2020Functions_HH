29b94a03c1aea4f6a72e555f861d049cbe1c1191
lzy-1006 <lzy-1006@berkeley.edu>
2020-11-01 18:20:53 -0800

    Restructured long line in peak_finding

--- lab1_lab2_essentials_peak_finding.py
+++ lab1_lab2_essentials_peak_finding.py
@@ -14,9 +14,12 @@ import numpy as np
 def peak_finding(signal_threshold_minimum, signal_data):
     signal_peaks = []
     signal_peaks_index = [] # x positions of the peaks, or rather, their index
+
     for i in range(len(signal_data) - 1): #len(signal)-1 because you will be checking the value after than your last i 
-        if (signal_threshold_minimum <= signal_data[i])             and (signal_data[i - 1] <= signal_data[i])             and (signal_data[i] >= signal_data[i + 1]):  #three conditions to be a peak 
+        if (signal_threshold_minimum <= signal_data[i]) \
+            and (signal_data[i - 1] <= signal_data[i]) \
+            and (signal_data[i] >= signal_data[i + 1]):  #three conditions to be a peak 
+            
             signal_peaks.append(signal_data[i])
             signal_peaks_index.append(i)
     return signal_peaks_index, signal_peaks
-
